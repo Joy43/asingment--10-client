2f8151aee083c2a4cda2cea804c2b2f6998dca47
joy43 <ssjoy43@gmail.com>
2023-11-04 21:36:42 +0600

    navbar setup by react router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
+ const naviteams=(
     <>
-   <li>
-    <Link to='/'> Home</Link>
-   </li>
-    
     <li>
-        <Link to="/contat">Contact</Link>
+     <Link to='/'> Home</Link>
     </li>
-</>
+     
+     <li>
+         <Link to="/contat">Contact</Link>
+     </li>
+ </>
+ )
     return (
         <div className="navbar bg-base-100">
       <div className="navbar-start">
