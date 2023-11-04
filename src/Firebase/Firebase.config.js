e851e637126ec8a7c730655bed2999d90eecc434
joy43 <ssjoy43@gmail.com>
2023-11-04 21:28:36 +0600

    facicon add and tailwind or daisyui setup

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLgCQt4YrGch_HOlEF8xNcb5TgCSWChgk",
+  authDomain: "assingment-11-hotel-mangement.firebaseapp.com",
+  projectId: "assingment-11-hotel-mangement",
+  storageBucket: "assingment-11-hotel-mangement.appspot.com",
+  messagingSenderId: "863370646568",
+  appId: "1:863370646568:web:aa1310750d6220b87b111d",
+  measurementId: "G-3VNFT3TGDR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default app;
