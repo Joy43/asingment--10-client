231f66c5c367de57c483853ccb0789a2477ff7c9
joy43 <ssjoy43@gmail.com>
2023-11-06 00:41:43 +0600

    error page setupt

--- src/Routers/Roter.jsx
+++ src/Routers/Roter.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Mainlayout from "../components/Layout/Mainlayout";
 import Home from "../pages/Home/Home";
 import Services from "../pages/ServicesHomepage/Services/Services";
+import ErrorPage from "../ErrorRouter/ErrorRouter";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
      element:<Mainlayout></Mainlayout>,
+     errorElement:<ErrorPage></ErrorPage>,
     children: [
         {
 path:'/',
