94ba31be102414763f6de0e2efd15ff2f72a2291
joy43 <ssjoy43@gmail.com>
2023-11-05 22:00:22 +0600

    aos animation adding

--- src/pages/ServicesHomepage/Services/Services.jsx
+++ src/pages/ServicesHomepage/Services/Services.jsx
@@ -1,29 +1,36 @@
+import { useEffect } from "react";
 import useServices from "../../../Hooks/useServices";
 import Servicescard from "../Servicescard/Servicescard";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const Services = () => {
+    //------------ load data------------
     const Serviceshome= useServices()
+    // ---------aos scroall----------
+    useEffect(() => {
+        AOS.init({ duration: 1000 }); // You can adjust the duration as needed
+      }, []);
+      
     return (
 
 
         <div>
             {/*-------- services ----------*/}
 
-            {/* <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        viewport={{ once: true }}
-      > */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 top-5 bottom-3" >
+      
+          <div  data-aos="fade-up" className="grid grid-cols-1 md:grid-cols-3 gap-6 top-5 bottom-3" >
             {
                 Serviceshome.map(Services=><Servicescard 
                 key={Services._id}
+               
+      
                 Services={Services}>
                 </Servicescard>)
             }
             </div>
-            {/* </motion.div>   */}
+         
         </div>
 
     );
